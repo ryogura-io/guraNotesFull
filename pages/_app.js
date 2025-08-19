deadaaee76acf76f043affbf48448908f52acda7
Gura <166049242+ryogura-io@users.noreply.github.com>
2025-08-19 16:40:06 +0100

    Add files via upload

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import '../styles/globals.css';
+import '../styles/bootstrap.min.css';
+import '../styles/bootstrap-icons.min.css';
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
