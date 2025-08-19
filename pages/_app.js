970b932ed5a86157bfef97ad1a06b47a0f4d81f1
Gura <166049242+ryogura-io@users.noreply.github.com>
2025-08-19 16:54:21 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
 import '../styles/globals.css';
 import '../styles/bootstrap.min.css';
-import '../styles/bootstrap-icons.min.css';
+// remove this if you want to use CDN instead
+// import '../styles/bootstrap-icons.min.css'; 
+
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        {/* Bootstrap Icons via CDN */}
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"
+          rel="stylesheet"
+        />
+      </Head>
+      
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+
