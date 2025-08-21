18d8f3022925541fd3db8a9ac9fcd0ca070db74b
Gura <166049242+ryogura-io@users.noreply.github.com>
2025-08-21 12:17:03 +0100

    Delete components directory

--- components/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer({ year }) {
-  return (
-    <footer className="text-secondary p-2">
-      <p className="text-center">&copy; {year} Gura-io Note Keeper</p>
-    </footer>
-  );
-}
